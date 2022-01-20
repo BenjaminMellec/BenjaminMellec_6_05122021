4fe241cb79677e52ce09d70d3eaaafc3e0992d17
Benjamin Mellec <benjamin.mellec@yousign.fr>
2022-01-20 18:29:15 +0100

    Add a loadingSpinner

---
+++ scripts/utils/loadingSpinner.js
@@ -0,0 +1,13 @@
+const body = document.querySelector("body");
+const loadingSpinner = document.getElementById("loading_spinner");
+
+if (loadingSpinner.classList.contains("loading-spinner__container--visible")) {
+  body.style.overflow = "hidden";
+}
+
+const hideLoadingSpinner = () => {
+  loadingSpinner.classList.remove("loading-spinner__container--visible");
+  body.style.overflow = "auto";
+};
+
+setTimeout(hideLoadingSpinner, 2500);
